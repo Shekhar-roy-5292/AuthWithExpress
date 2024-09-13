b8f5e1da03c8a98aaadc054cff27356ccdc80276
Shekhar Roy <43708251+Shekhar-roy-5292@users.noreply.github.com>
2024-09-13 18:07:24 +0530

    add cors

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const app = express();
 const authRouter = require("./router/authRoute.js");
 const databaseConnect = require("./config/databaseConfig.js");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 databaseConnect();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: [process.env.CLIENT_URL],
+  credentials: true,
+  // allowedHeaders: ["Content-Type", "Authorization"],
+  // exposedHeaders: ["Authorization"]  // expose Authorization header in the response
+}));
 app.use("/api/auth", authRouter);
 app.use("/", (req, res) => {
   res.status(200).json({ data: "Hello World!" });
